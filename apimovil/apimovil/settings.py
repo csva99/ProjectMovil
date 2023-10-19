85adbc255643fa342ea66ed97b226b7e5f2e786f
Cristopher <csva.99@outlook.com>
2023-10-19 00:45:24 -0300

    modified:   apimovil/apimovil/__pycache__/__init__.cpython-39.pyc 	modified:   apimovil/apimovil/__pycache__/settings.cpython-39.pyc 	modified:   apimovil/apimovil/__pycache__/urls.cpython-39.pyc 	modified:   apimovil/apimovil/__pycache__/wsgi.cpython-39.pyc 	modified:   apimovil/apimovil/settings.py 	modified:   apimovil/core/__pycache__/__init__.cpython-39.pyc 	modified:   apimovil/core/__pycache__/admin.cpython-39.pyc 	modified:   apimovil/core/__pycache__/apps.cpython-39.pyc 	modified:   apimovil/core/__pycache__/models.cpython-39.pyc 	modified:   apimovil/core/migrations/__pycache__/0001_initial.cpython-39.pyc 	modified:   apimovil/core/migrations/__pycache__/0002_remove_usuario_activo.cpython-39.pyc 	modified:   apimovil/core/migrations/__pycache__/__init__.cpython-39.pyc 	modified:   apimovil/rest_api/__pycache__/__init__.cpython-39.pyc 	modified:   apimovil/rest_api/__pycache__/admin.cpython-39.pyc 	modified:   apimovil/rest_api/__pycache__/apps.cpython-39.pyc 	modified:   apimovil/rest_api/__pycache__/models.cpython-39.pyc 	modified:   apimovil/rest_api/__pycache__/serializers.cpython-39.pyc 	modified:   apimovil/rest_api/__pycache__/urls.cpython-39.pyc 	modified:   apimovil/rest_api/__pycache__/views.cpython-39.pyc 	modified:   apimovil/rest_api/migrations/__pycache__/__init__.cpython-39.pyc

--- apimovil/apimovil/settings.py
+++ apimovil/apimovil/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "core",
     "rest_api",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "apimovil.urls"
@@ -125,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8100",  # Reemplaza con la URL de tu aplicación Angular
+]
